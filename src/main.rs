442111de60f5fff0201ab6de5061980d8bc3913c
objektwerks <objektwerks@runbox.com>
2021-04-15 17:14:00 -0400

    added spawned threads to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
     const MESSAGE: &str = "Hello, Rust!";
     println!("{}", MESSAGE);
+
+    use std::thread;
+    use std::time::Duration;
+
+    let handle = thread::spawn(|| {
+        for i in 1..4 {
+            println!("Hi! I'm thread #{}!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    handle.join().unwrap();
 }
 
 #[cfg(test)]
