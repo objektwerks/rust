45e2c446b3a4f35699c7674906abe57f77815436
objektwerks <objektwerks@runbox.com>
2021-04-15 08:01:12 -0400

    added hashmap test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let rust = "Rust!";
-    println!("Hello, {}", rust);
+    let rust = "Hello, Rust!";
+    println!("{}", rust);
 }
 
 #[cfg(test)]
@@ -32,6 +32,17 @@ mod tests {
         hs.insert(1);
         hs.insert(2);
         hs.insert(3);
-        assert!( hs.contains(&3));
+        assert!( hs.contains(&3) );
+    }
+
+    #[test]
+    fn hashmap() {
+        use std::collections::HashMap;
+
+        let mut hm = HashMap::new();
+        hm.insert(1, 1);
+        hm.insert(2, 2);
+        hm.insert(3, 3);
+        assert_eq!( hm.get(&3), Some(&3) );
     }
 }
