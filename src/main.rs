015a2fa5844a41f30c225da84af756db83adda7a
objektwerks <objektwerks@runbox.com>
2021-04-15 17:37:00 -0400

    added option test

--- src/main.rs
+++ src/main.rs
@@ -141,10 +141,20 @@ mod tests {
             };
             v
         }
-        assert_eq!(square(1), 1);
-        assert_eq!(square(2), 4);
-        assert_eq!(square(3), 9);
-        assert_eq!(square(4), 0);
+        assert_eq!( square(1), 1 );
+        assert_eq!( square(2), 4 );
+        assert_eq!( square(3), 9 );
+        assert_eq!( square(4), 0 );
+    }
+
+    #[test]
+    fn option() {
+        fn to_int(s: &str) -> Option<i32> {
+            let i: Option<i32> = s.parse::<i32>().ok();
+            i
+        }
+        assert_eq!( to_int("3"), Some(3) );
+        assert_eq!( to_int("c"), None );
     }
 
     #[test]
