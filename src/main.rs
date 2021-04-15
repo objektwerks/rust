2f7b23001a632ea67da7ece11e76756e461878bc
objektwerks <objektwerks@runbox.com>
2021-04-15 07:28:02 -0400

    added unit test to main

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,13 @@ fn main() {
     let rust = "Rust!";
     println!("Hello, {}", rust);
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn array() {
+        let mut v = [1, 2, 3];
+        v.reverse();
+        assert_eq!( v, [3, 2, 1] );
+    }
+}
