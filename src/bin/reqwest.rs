55339c5d679d2d42bded9956d22f462d381a180e
objektwerks <objektwerks@runbox.com>
2021-04-16 07:14:14 -0400

    corrected reqwest code; it now works

--- src/bin/reqwest.rs
+++ src/bin/reqwest.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
 use std::error::Error;
 use reqwest::blocking::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let response = get("http://api.icndb.com/jokes/random/")?
-                   .json::<HashMap<String, String>>()?;
-    println!("{:#?}", response);
+    let response = get("http://api.icndb.com/jokes/random/")?.text()?;
+    println!("body = {:?}", response);
     Ok(())
 }
