cab510a148e2126894000170e09d277d944f26db
objektwerks <objektwerks@runbox.com>
2021-04-16 08:31:29 -0400

    reformatted hyper code

--- src/bin/hyper.rs
+++ src/bin/hyper.rs
@@ -16,9 +16,9 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let make_svc = make_service_fn( |_conn| {
         async { Ok::<_, Infallible>( service_fn(now) ) }
     });
-
     let addr = ([127, 0, 0, 1], 7979).into();
     let server = Server::bind(&addr).serve(make_svc);
+
     println!("Listening on http://{}", addr);
     server.await?;
     Ok(())
