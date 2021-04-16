65452b8d6d5986635bb5729f56557a9d08ba14f4
objektwerks <objektwerks@runbox.com>
2021-04-16 08:25:48 -0400

    refactored hyper server to return current utc datetime

--- src/bin/hyper.rs
+++ src/bin/hyper.rs
@@ -1,18 +1,20 @@
 #![deny(warnings)]
 
 use std::convert::Infallible;
+use chrono::Utc;
+
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello, Rust!")))
+async fn now(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok( Response::new( Body::from( Utc::now().to_string() ) ) )
 }
 
 #[tokio::main]
 /** To test server: curl http://localhost:7979/ */
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let make_svc = make_service_fn( |_conn| {
-        async { Ok::<_, Infallible>( service_fn(hello) ) }
+        async { Ok::<_, Infallible>( service_fn(now) ) }
     });
 
     let addr = ([127, 0, 0, 1], 7979).into();
