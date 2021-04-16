bdd4be750a239f4a1e344592f00801f3714d231a
objektwerks <objektwerks@runbox.com>
2021-04-16 07:53:47 -0400

    enhanced result test

--- tests/tests.rs
+++ tests/tests.rs
@@ -232,6 +232,15 @@ mod tests {
         let mut contents = String::new();
         file.read_to_string(&mut contents).unwrap();
         assert!( !contents.is_empty() );
+
+        use std::io::Error;
+        fn read_file(file: &str) -> Result<String, Error> {
+            let mut f = File::open(file)?;
+            let mut s = String::new();
+            f.read_to_string(&mut s)?;
+            Ok(s)
+        }
+        assert!( read_file("Cargo.toml").ok().is_some() )
     }
 
     #[test]
