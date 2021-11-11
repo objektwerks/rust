3bad4935b73975ecd8616b493f49ffefbc652ae8
objektwerks <objektwerks@runbox.com>
2021-11-11 05:53:52 -0500

    reordered main macro and rust doc

--- src/bin/hyper.rs
+++ src/bin/hyper.rs
@@ -11,8 +11,9 @@ async fn now(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new(Body::from(Utc::now().to_string())))
 }
 
-#[tokio::main]
+
 /** To test server: curl http://localhost:7878/ */
+#[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(now)) });
     let addr = ([127, 0, 0, 1], 7878).into();
