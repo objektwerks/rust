ed24c6e07889a0e050b0b142dece38b9235b360e
objektwerks <objektwerks@runbox.com>
2021-11-11 05:02:53 -0500

    refactored imports and http address

--- src/bin/hyper.rs
+++ src/bin/hyper.rs
@@ -1,22 +1,21 @@
 #![deny(warnings)]
 
-use std::convert::Infallible;
-
 use chrono::Utc;
-use hyper::{Body, Request, Response, Server};
+
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+
+use std::convert::Infallible;
 
 async fn now(_: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new(Body::from(Utc::now().to_string())))
 }
 
 #[tokio::main]
-/** To test server: curl http://localhost:7979/ */
+/** To test server: curl http://localhost:7878/ */
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(now)) }
-    });
-    let addr = ([127, 0, 0, 1], 7979).into();
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(now)) });
+    let addr = ([127, 0, 0, 1], 7878).into();
     let server = Server::bind(&addr).serve(make_svc);
 
     println!("Listening on http://{}", addr);
