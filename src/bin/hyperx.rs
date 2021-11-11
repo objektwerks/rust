68b698e965b452e982a9d96995fbe66dda27b28b
objektwerks <objektwerks@runbox.com>
2021-11-11 05:03:08 -0500

    refactored imports and http address

--- src/bin/hyperx.rs
+++ src/bin/hyperx.rs
@@ -1,8 +1,9 @@
 #![deny(warnings)]
 
 use chrono::Utc;
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 async fn now(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
@@ -16,12 +17,10 @@ async fn now(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 }
 
 #[tokio::main]
-/** To test server: curl http://localhost:7878/now */
+/** To test server: curl http://localhost:7979/now */
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 7878).into();
-    let service = make_service_fn(|_| async {
-        Ok::<_, hyper::Error>(service_fn(now))
-    });
+    let addr = ([127, 0, 0, 1], 7979).into();
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(now)) });
     let server = Server::bind(&addr).serve(service);
 
     println!("Listening on http://{}", addr);
