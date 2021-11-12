061cff4d2711b1efe7ce3447bb5e115cb24fbec9
objektwerks <objektwerks@runbox.com>
2021-11-12 01:44:53 -0500

    added recursion

---
+++ tests/recursion.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod recursion {
+    #[test]
+    fn recursion() {
+        fn factorial(n: u32, acc: u32) -> u32 {
+            if n == 1 {
+                return acc;
+            } else {
+                factorial(n - 1, acc * n)
+            }
+        }
+        assert_eq!(factorial(9, 1), 362880)
+    }
+}
