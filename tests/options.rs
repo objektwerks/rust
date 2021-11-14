88b375bf2115257cc4d46f32447b3ebe0e65c262
objektwerks <objektwerks@runbox.com>
2021-11-14 17:00:47 -0500

    crefactored fn in options test

--- tests/options.rs
+++ tests/options.rs
@@ -3,15 +3,15 @@ mod options {
     #[test]
     fn option() {
         fn to_int(s: &str) -> Option<i32> {
-            let i: Option<i32> = s.parse::<i32>().ok();
-            i
+            return s.parse::<i32>().ok();
         }
+        
         assert_eq!(to_int("3").unwrap_or(0), 3);
         assert_eq!(to_int("c").unwrap_or(0), 0);
 
-        let b = ["1", "a", "2", "3"];
+        let ns = ["1", "a", "2", "3"];
 
-        assert_eq!( b.iter().map(|s| to_int(s)).flatten().sum::<i32>(), 6 );
-        assert_eq!( b.iter().flat_map(|s| to_int(s)).sum::<i32>(), 6 );
+        assert_eq!( ns.iter().map(|s| to_int(s)).flatten().sum::<i32>(), 6 );
+        assert_eq!( ns.iter().flat_map(|s| to_int(s)).sum::<i32>(), 6 );
     }
 }
