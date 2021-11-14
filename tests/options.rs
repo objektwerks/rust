833d10e85edac62c8f316633d0e15ed8367994ec
objektwerks <objektwerks@runbox.com>
2021-11-14 14:06:36 -0500

    enhanced option test

--- tests/options.rs
+++ tests/options.rs
@@ -8,5 +8,10 @@ mod options {
         }
         assert_eq!(to_int("3").unwrap_or(0), 3);
         assert_eq!(to_int("c").unwrap_or(0), 0);
+
+        let b = ["1", "a", "2", "3"];
+
+        assert_eq!( b.iter().map(|s| to_int(s)).flatten().sum::<i32>(), 6 );
+        assert_eq!( b.iter().flat_map(|s| to_int(s)).sum::<i32>(), 6 );
     }
 }
