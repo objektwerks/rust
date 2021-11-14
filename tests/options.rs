bac9125190075279096ba4cce3313e268cd5fa6d
objektwerks <objektwerks@runbox.com>
2021-11-14 08:34:53 -0500

    added unwrap_or to option tests

--- tests/options.rs
+++ tests/options.rs
@@ -6,7 +6,7 @@ mod options {
             let i: Option<i32> = s.parse::<i32>().ok();
             i
         }
-        assert_eq!(to_int("3"), Some(3));
-        assert_eq!(to_int("c"), None);
+        assert_eq!(to_int("3").unwrap_or(0), 3);
+        assert_eq!(to_int("c").unwrap_or(0), 0);
     }
 }
