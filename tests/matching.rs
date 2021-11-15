aef33067c6d98af7430570746752c3eafe2b90b4
objektwerks <objektwerks@runbox.com>
2021-11-15 18:56:54 -0500

    added new fn to matching test

--- tests/matching.rs
+++ tests/matching.rs
@@ -20,6 +20,9 @@ mod matching {
             y: i32,
         }
         impl Point {
+            fn new(x: i32, y: i32) -> Point {
+                Point { x, y }
+            }
             fn match_on_point(&self) -> String {
                 match self {
                     Point { x: 1, y: _ } => format!("match on x ({}, {})", self.x, self.y),
@@ -29,13 +32,13 @@ mod matching {
             }
         }
 
-        let px = &Point { x: 1, y: 1 };
+        let px = &Point::new(1, 1);
         assert_eq!(px.match_on_point(), "match on x (1, 1)" );
 
-        let py = &Point { x: 2, y: 2 };
+        let py = &Point::new(2, 2);
         assert_eq!(py.match_on_point(), "match on y (2, 2)" );
 
-        let pxy = &Point { x: 3, y: 3 };
+        let pxy = &Point::new(3, 3);
         assert_eq!(pxy.match_on_point(), "default: (3, 3)" );
     }
 }
