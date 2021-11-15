dd3ef2b86d23c201f07e7423087bd842ca7e9bb3
objektwerks <objektwerks@runbox.com>
2021-11-15 17:15:04 -0500

    refactored matchings

--- tests/matching.rs
+++ tests/matching.rs
@@ -5,14 +5,10 @@ mod matching {
         fn square(i: i32) -> i32 {
             match i {
                 1 => 1 * 1,
-                2 => 2 * 2,
-                3 => 3 * 3,
-                _ => 0
+                @n => n * n,
             }
         }
         assert_eq!( square(1), 1 );
-        assert_eq!( square(2), 4 );
-        assert_eq!( square(3), 9 );
-        assert_eq!( square(4), 0 );
+        assert_eq!( square(4), 16 );
     }
 }
