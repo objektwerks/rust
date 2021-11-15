cb91b726c3992d98f122faabbe38b6ad160b55fb
objektwerks <objektwerks@runbox.com>
2021-11-15 18:45:37 -0500

    refactored structs function in matching test

--- tests/matching.rs
+++ tests/matching.rs
@@ -19,16 +19,21 @@ mod matching {
             y: i32,
         }
         impl Point {
-            fn match_on_point(p: &Point) -> String {
-                match p {
-                    Point { x: 1, y: _ } => format!("match on x ({}, {})", p.x, p.y),
-                    Point { x: _, y: 2 } => format!("match on y ({}, {})", p.x, p.y),
-                    Point { x: _, y: _ } => format!("default: ({}, {})", p.x, p.y),
+            fn match_on_point(&self) -> String {
+                match self {
+                    Point { x: 1, y: _ } => format!("match on x ({}, {})", self.x, self.y),
+                    Point { x: _, y: 2 } => format!("match on y ({}, {})", self.x, self.y),
+                    Point { x: _, y: _ } => format!("default: ({}, {})", self.x, self.y),
                 }
             }
         }
-        assert_eq!(Point::match_on_point( &Point { x: 1, y: 1 } ), "match on x (1, 1)" );
-        assert_eq!(Point::match_on_point( &Point { x: 2, y: 2 } ), "match on y (2, 2)" );
-        assert_eq!(Point::match_on_point( &Point { x: 3, y: 3 } ), "default: (3, 3)" );
+        let px = &Point { x: 1, y: 1 };
+        assert_eq!(px.match_on_point(), "match on x (1, 1)" );
+
+        let py = &Point { x: 2, y: 2 };
+        assert_eq!(py.match_on_point(), "match on y (2, 2)" );
+
+        let pxy = &Point { x: 3, y: 3 };
+        assert_eq!(pxy.match_on_point(), "default: (3, 3)" );
     }
 }
