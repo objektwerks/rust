66212b6116e4c6a8ed9402c4804123895c75f477
objektwerks <objektwerks@runbox.com>
2021-11-15 18:03:58 -0500

    renamed point function name

--- tests/matching.rs
+++ tests/matching.rs
@@ -19,7 +19,7 @@ mod matching {
             y: i32,
         }
         impl Point {
-            fn match_points(p: &Point) -> String {
+            fn match_on_point(p: &Point) -> String {
                 match p {
                     Point { x: 1, y: _ } => format!("match on x ({}, {})", p.x, p.y),
                     Point { x: _, y: 2 } => format!("match on y ({}, {})", p.x, p.y),
@@ -27,8 +27,8 @@ mod matching {
                 }
             }
         }
-        assert_eq!( Point::match_points( &Point { x: 1, y: 1 } ), "match on x (1, 1)" );
-        assert_eq!( Point::match_points( &Point { x: 2, y: 2 } ), "match on y (2, 2)" );
-        assert_eq!( Point::match_points( &Point { x: 3, y: 3 } ), "default: (3, 3)" );
+        assert_eq!(Point::match_on_point( &Point { x: 1, y: 1 } ), "match on x (1, 1)" );
+        assert_eq!(Point::match_on_point( &Point { x: 2, y: 2 } ), "match on y (2, 2)" );
+        assert_eq!(Point::match_on_point( &Point { x: 3, y: 3 } ), "default: (3, 3)" );
     }
 }
