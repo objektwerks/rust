db934ad272725c5ddc58328e84ac5bc3e5960c76
objektwerks <objektwerks@runbox.com>
2021-11-15 18:40:17 -0500

    refactored structs test

--- tests/structs.rs
+++ tests/structs.rs
@@ -3,11 +3,19 @@ mod structs {
     #[test]
     fn structure() {
         struct Person {
-            name: String,
+            first: String,
+            last: String,
             age: u32,
         }
-        let p = Person { name: String::from("Fred Flintstone"), age: 27 };
-        assert_eq!(p.name, "Fred Flintstone");
-        assert_eq!(p.age, 27);
+        impl Person {
+            fn name(&self) -> String {
+                return self.first.to_string() + " " + &self.last.to_string();
+            }
+        }
+        let p = Person { first: "Fred".to_string(), last: "Flintstone".to_string(), age: 27 };
+        assert_eq!( p.first, "Fred" );
+        assert_eq!( p.last, "Flintstone" );
+        assert_eq!( p.age, 27 );
+        assert_eq!( p.name(), "Fred Flintstone" )
     }
 }
