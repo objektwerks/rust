79a61b4ff0efd5021bf4283c315dad548adf4a85
objektwerks <objektwerks@runbox.com>
2021-11-16 07:06:54 -0500

    added fn pointer test to functions test

--- tests/functions.rs
+++ tests/functions.rs
@@ -9,4 +9,25 @@ mod functions {
         let a = [1, 2, 3];
         assert_eq!( a.iter().map(|x| square(*x)).last().unwrap_or(0), 9 );
     }
+
+    #[test]
+    fn fn_pointer() {
+        fn sum(x: u32, y: u32) -> u32 {
+            x + y
+        }
+
+        fn product(x: u32, y: u32) -> u32 {
+            x * y
+        }
+
+        fn combine(f: fn(u32, u32) -> u32,
+                   g: fn(u32, u32) -> u32,
+                   x: u32,
+                   y: u32) -> u32 {
+            f(x, y) + g(x, y)
+        }
+
+        let result = combine(sum, product, 3, 3);
+        assert_eq!( result, 15 )
+    }
 }
