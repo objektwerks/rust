749acd6f8f54134926a39bc3d0d108b19b6ff0ce
objektwerks <objektwerks@runbox.com>
2021-11-15 19:47:33 -0500

    enhanced enums test

--- tests/enums.rs
+++ tests/enums.rs
@@ -1,23 +1,30 @@
 #[cfg(test)]
 mod enums {
     #[test]
-    fn enumeration() {
+    fn enums() {
         #[derive(PartialEq, Eq)]
         enum Gender {
             Male,
             Female,
         }
+
         struct Worker {
             name: String,
             age: u32,
             gender: Gender,
         }
-        let m = Worker { name: String::from("Barney Rebel"), age: 26, gender: Gender::Male };
+        impl Worker {
+            fn new(name: String, age: u32, gender: Gender) -> Worker {
+                Worker { name, age, gender }
+            }
+        }
+
+        let m = Worker::new(String::from("Barney Rebel"), 26, Gender::Male);
         assert_eq!(m.name, "Barney Rebel");
         assert_eq!(m.age, 26);
         assert!(m.gender.eq(&Gender::Male));
 
-        let f = Worker { name: String::from("Betty Rebel"), age: 26, gender: Gender::Female };
+        let f = Worker::new(String::from("Betty Rebel"), 26, Gender::Female);
         assert_eq!(f.name, "Betty Rebel");
         assert_eq!(f.age, 26);
         assert!(f.gender.eq(&Gender::Female));
