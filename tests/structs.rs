f435cb830ba17f61d0dc17a61c758aba18a75853
objektwerks <objektwerks@runbox.com>
2021-11-16 07:47:41 -0500

    added tuple_structs test to structs

--- tests/structs.rs
+++ tests/structs.rs
@@ -22,4 +22,13 @@ mod structs {
         assert_eq!( p.age, 27 );
         assert_eq!( p.name(), "Fred Flintstone" )
     }
+
+    #[test]
+    fn tuple_structs() {
+        #[derive(PartialEq, Debug)]
+        struct Color(i32, i32, i32);
+
+        const NAVY: Color = Color(0, 0, 128);
+        assert_eq!( NAVY, Color(0, 0, 128) )
+    }
 }
