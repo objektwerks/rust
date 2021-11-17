0c610ad702ac9a908cae9e4ee4eca9b98dcd8014
objektwerks <objektwerks@runbox.com>
2021-11-17 11:47:32 -0500

    added thread test in concurrency test

--- tests/concurrency.rs
+++ tests/concurrency.rs
@@ -1,7 +1,16 @@
 #[cfg(test)]
 mod concurrency {
     #[test]
-    fn concurrency() {
-        assert_eq!(true, true);
+    fn thread() {
+        use std::thread;
+        use std::time::Duration;
+
+        let handle = thread::spawn(|| {
+            for i in 1..11 {
+                assert_eq!( i, i );
+                thread::sleep(Duration::from_millis(i));
+            }
+        });
+        handle.join().unwrap();
     }
 }
