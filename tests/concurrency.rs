910b8fa1f5d106ceeb3d516353de8f529660de2f
objektwerks <objektwerks@runbox.com>
2021-11-18 08:15:53 -0500

    added mutex test

--- tests/concurrency.rs
+++ tests/concurrency.rs
@@ -33,12 +33,36 @@ mod concurrency {
 
         let (producer, consumer) = mpsc::channel();
 
-        thread::spawn(move || {
+        thread::spawn( move || {
             let message = 1;
             producer.send(message).unwrap();
         });
 
         let message = consumer.recv().unwrap();
         assert_eq!(message, 1 )
+    }
+
+    #[test]
+    fn mutex() {
+        use std::sync::{Arc, Mutex};
+        use std::thread;
+
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn( move || {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        assert_eq!( *counter.lock().unwrap(), 10 );
     }
 }
