db8334553d18af97fecf3b54cbc588bbe21cadce
objektwerks <objektwerks@runbox.com>
2021-11-18 09:21:59 -0500

    added deref test

--- tests/pointers.rs
+++ tests/pointers.rs
@@ -1,8 +1,16 @@
 #[cfg(test)]
 mod pointers {
     #[test]
+    fn deref() {
+        let x = 1;
+        let y = &x;
+        assert_eq!( x, 1 );
+        assert_eq!( *y, 1 );
+    }
+
+    #[test]
     fn box_sp() {
-        let box_sp = Box::new(3);
-        assert_eq!( *box_sp, 3 );
+        let box_sp = Box::new(1);
+        assert_eq!( *box_sp, 1 );
     }
 }
