5ef616032f40c3f9abe052092fb18522ef8e84d9
objektwerks <objektwerks@runbox.com>
2021-11-18 11:22:46 -0500

    enhanced vector test

--- tests/collections.rs
+++ tests/collections.rs
@@ -40,6 +40,13 @@ mod collections {
     fn vector() {
         let v = vec![1, 2, 3];
         assert_eq!( v.len(), 3 );
+
+        let mut mv = vec![];
+        mv.push(1);
+        mv.push(2);
+        mv.push(3);
+        assert_eq!( mv.len(), 3 );
+        assert_eq!( v, mv );
     }
 
     #[test]
