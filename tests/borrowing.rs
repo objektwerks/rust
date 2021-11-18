f2e46ec536edc2a92e344b643f1318cd67da90d4
objektwerks <objektwerks@runbox.com>
2021-11-18 10:09:08 -0500

    added ref_mutable test

--- tests/borrowing.rs
+++ tests/borrowing.rs
@@ -1,14 +1,26 @@
 #[cfg(test)]
 mod borrowing {
     #[test]
-    fn reference() {
+    fn ref_immutable() {
         fn calc_len(string: &String) -> usize { // borrower
             string.len()
         }
 
         let value = String::from("value"); // owner
-        let len = calc_len( &value ); // borrower
+        let len = calc_len( &value );
 
         assert_eq!( len, 5 );
+    }
+
+    #[test]
+    fn ref_mutable() {
+        fn mutate(string: &mut String) { // borrower
+            string.push_str(", value");
+        }
+
+        let mut value = String::from("value"); // owner
+        mutate( &mut value );
+
+        assert_eq!( value, "value, value".to_string() );
     }
 }
