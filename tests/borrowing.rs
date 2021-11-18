7d22b457c5fd84dfd2b66b0ea7836621523d2ccd
objektwerks <objektwerks@runbox.com>
2021-11-18 10:02:11 -0500

    added comments

--- tests/borrowing.rs
+++ tests/borrowing.rs
@@ -2,12 +2,12 @@
 mod borrowing {
     #[test]
     fn reference() {
-        fn calc_len(string: &String) -> usize {
+        fn calc_len(string: &String) -> usize { // borrower
             string.len()
         }
 
-        let value = String::from("value");
-        let len = calc_len( &value);
+        let value = String::from("value"); // owner
+        let len = calc_len( &value ); // borrower
 
         assert_eq!( len, 5 );
     }
