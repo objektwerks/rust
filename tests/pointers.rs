a8ed56f07180e2ec0c36c7a94966f9565fe1e85e
objektwerks <objektwerks@runbox.com>
2021-11-18 09:16:52 -0500

    added box smart pointer test

--- tests/pointers.rs
+++ tests/pointers.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod pointers {
     #[test]
-    fn pointers() {
-        assert_eq!(true, true);
+    fn box_sp() {
+        let box_sp = Box::new(3);
+        assert_eq!( *box_sp, 3 );
     }
 }
