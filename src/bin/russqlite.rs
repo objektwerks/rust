19f7420f4246724524c6fd6494b38bad0c7bb33a
objektwerks <objektwerks@runbox.com>
2021-11-19 13:59:22 -0500

    added print fn to Todo

--- src/bin/russqlite.rs
+++ src/bin/russqlite.rs
@@ -16,6 +16,27 @@ impl Todo {
             params![ self.task ],
         )
     }
+    fn list(connection: &Connection) -> Result<Vec<Todo>> {
+        let mut select_todos = connection.prepare("SELECT id, task FROM todo")?;
+        let todos = select_todos.query_map([], |row| {
+            Ok(
+                Todo {
+                    id: row.get(0)?,
+                    task: row.get(1)?,
+                }
+            )
+        })?;
+        let mut list = Vec::new();
+        for todo in todos {
+            list.push(todo?);
+        }
+        Ok(list)
+    }
+    fn print(todos: Result<Vec<Todo>>) -> () {
+        for todo in todos.unwrap() {
+            println!("selected: {:?}", todo);
+        }
+    }
 }
 
 fn main() -> Result<()> {
@@ -29,21 +50,10 @@ fn main() -> Result<()> {
     )?;
 
     let todo = Todo::new( "mow yard".to_string() );
-    let rows = Todo::insert(&todo, &connection)?;
+    let rows = Todo::insert( &todo, &connection )?;
     println!("inserted: {} {:?}", rows, todo);
 
-    let mut select_todos = connection.prepare("SELECT id, task FROM todo")?;
-    let todos = select_todos.query_map([], |row| {
-        Ok(
-            Todo {
-                id: row.get(0)?,
-                task: row.get(1)?,
-            }
-        )
-    })?;
-    for todo in todos {
-        println!("selected: {:?}", todo.unwrap());
-    }
+    Todo::print( Todo::list(&connection) );
 
     Ok(())
 }
