14918571897e7e99803e1fea9118255c5d182987
objektwerks <objektwerks@runbox.com>
2021-11-21 11:10:05 -0500

    removed logging from main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,7 @@
-use log::info;
-use log4rs;
-
-use std::env;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
-    info!("log4rs logger initialized!");
-
-    let args: Vec<String> = env::args().collect();
-    let current_dir = env::current_dir();
-    info!("args: {:?}", args);
-    info!("current dir: {:?} ", current_dir.unwrap().display());
-
-    let key = "key";
-    let value = "value";
-    info!("pre-set -> key: {} value: {}", key, value);
-    env::set_var(key, value);
-    info!("post-set -> key: {} value: {:?}", key, env::var(key));
-
     const MESSAGE: &str = "Hello, Rust!";
     println!("{}", MESSAGE);
 
@@ -37,8 +19,4 @@ fn main() {
     }
 
     handle.join().unwrap();
-
-    for (key, value) in env::vars_os() {
-        info!("{:?}: {:?}", key, value);
-    }
 }
