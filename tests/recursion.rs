8e15fda85432711f72f20b0e3cf67ccb23ccc7bc
objektwerks <objektwerks@runbox.com>
2021-11-23 13:17:13 -0500

    added fibonacci test

--- tests/recursion.rs
+++ tests/recursion.rs
@@ -9,6 +9,17 @@ mod recursion {
                 factorial(n - 1, acc * n)
             }
         }
-        assert_eq!(factorial(9, 1), 362880)
+        assert_eq!( factorial(9, 1), 362880 )
+    }
+
+    #[test]
+    fn fibonacci() {
+        fn fibonacci(n: i32, a: i32, b: i32) -> i32 {
+            match n {
+                0 => a,
+                _ => fibonacci(n - 1, b, a + b),
+            }
+        }
+        assert_eq!( fibonacci(9, 0, 1), 34 );
     }
 }
