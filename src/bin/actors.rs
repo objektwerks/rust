a0abfc8c7d7c4a89dcbd771dca568c4668d76992
objektwerks <objektwerks@runbox.com>
2021-11-24 05:41:21 -0500

    refactored actors

--- src/bin/actors.rs
+++ src/bin/actors.rs
@@ -1,5 +1,6 @@
 use actix::prelude::*;
 
+#[derive(Debug)]
 struct CountActor {
     count: usize,
 }
@@ -24,8 +25,13 @@ impl Handler<Count> for CountActor {
 
 #[actix_rt::main]
 async fn main() {
-    let count_actor_address = CountActor { count: 0 }.start();
+    let count_actor = CountActor { count: 0 };
+    println!("count actor state: {:?}", count_actor);
+
+    let count_actor_address = count_actor.start();
+
     let count_actor_response = count_actor_address.send( Count(1) ).await;
-    println!("count actor response is: {}", count_actor_response.unwrap());
+    println!("count actor response and state: {}", count_actor_response.unwrap());
+
     System::current().stop();
 }
